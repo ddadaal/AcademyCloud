af3d2284219464fe6776c5e8d38f58db3fb364d4
Chen Junda <ddadaal@outlook.com>
2020-01-30 21:38:09 +0800

    fix provisioning

--- preparation/openstack/scripts/prerequisites.sh
+++ preparation/openstack/scripts/prerequisites.sh
@@ -1,6 +1,6 @@
+
 echo "Set tuna as apt mirror"
-sudo cp /etc/apt/sources.list /etc/apt/sources.list.bk
-sudo cat /vagrant/scripts/sources.list > /etc/apt/sources.list
+sudo mv /tmp/sources.list /etc/apt/sources.list
 
 echo "Update apt and install openstackclient"
 sudo apt update
