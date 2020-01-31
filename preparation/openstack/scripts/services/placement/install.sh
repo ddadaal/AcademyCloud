a47cb382b0a634636f425125d2dcab962a1ae2fd
Chen Junda <ddadaal@outlook.com>
2020-01-31 17:49:55 +0800

    placement working

--- preparation/openstack/scripts/services/placement/install.sh
+++ preparation/openstack/scripts/services/placement/install.sh
@@ -13,15 +13,11 @@ openstack endpoint create --region RegionOne placement public http://controller:
 openstack endpoint create --region RegionOne placement internal http://controller:8778
 openstack endpoint create --region RegionOne placement admin http://controller:8778
 
-# Install placement through pip
-
-pip3 install openstack-placement pymysql
-mkdir /etc/placement
+yum install -y openstack-placement-api
 cp "$SH_DIR/placement.conf" /etc/placement/
-placement-manage db sync
+su -s /bin/sh -c "placement-manage db sync" placement
 
-# Configure Apache2
+# https://ask.openstack.org/en/question/122022/stein-expecting-value-line-1-column-1-char-0-oscplacement/
+cp "$SH_DIR/00-placement-api.conf" /etc/httpd/conf.d/
 
-cp "$SH_DIR/apache-placement.conf" /etc/apache2/sites-available/placement.conf
-ln -s /etc/apache2/sites-available/placement.conf /etc/apache2/sites-enabled/placement.conf
-service apache2 restart
+systemctl restart httpd
--- preparation/openstack/scripts/services/placement/verify.sh
+++ preparation/openstack/scripts/services/placement/verify.sh
@@ -1,8 +1,7 @@
 . /root/admin-openrc
 placement-status upgrade check
 
-pip3 install --ignore-installed simplejson
-pip3 install osc-placement
+yum install -y python-osc-placement
 
 openstack --os-placement-api-version 1.2 resource class list --sort-column name
 openstack --os-placement-api-version 1.6 trait list --sort-column name
