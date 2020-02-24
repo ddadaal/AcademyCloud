75615189459d7eabcd3961b128298bedf209fedc
Chen Junda <ddadaal@outlook.com>
2020-02-24 23:03:51 +0800

    improve python docker

--- sources/backend/src/Resources/Resources/app.py
+++ sources/backend/src/Resources/Resources/app.py
@@ -2,9 +2,13 @@ import grpc
 import services
 from concurrent.futures import ThreadPoolExecutor
 
+port = 50052
+
 server = grpc.server(ThreadPoolExecutor(max_workers=10))
 services.init_services(server)
 
-server.add_insecure_port('[::]:50051')
+print(f'Server running at localhost:{port}')
+
+server.add_insecure_port(f'[::]:{port}')
 server.start()
 server.wait_for_termination()
