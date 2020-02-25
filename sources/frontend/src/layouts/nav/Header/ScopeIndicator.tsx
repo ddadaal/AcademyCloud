0a61f267be7e802d80591ad20309d064c2006516
Chen Junda <ddadaal@outlook.com>
2020-02-25 19:48:21 +0800

    fix scopeindicator id problem

--- sources/frontend/src/layouts/nav/Header/ScopeIndicator.tsx
+++ sources/frontend/src/layouts/nav/Header/ScopeIndicator.tsx
@@ -1,13 +1,14 @@
-import React, { useState } from "react";
+import React, { useState, Fragment } from "react";
 import { useStore } from 'simstate';
 import { UserStore } from 'src/stores/UserStore';
-import { Menu, Dropdown, notification } from "antd";
+import { Menu, Dropdown } from "antd";
 import { DownOutlined, BookOutlined } from "@ant-design/icons";
-import { lang, Localized, I18nStore } from "src/i18n";
+import { lang, Localized } from "src/i18n";
 import { scopeId, scopeName, Scope, isSystemScope } from "src/models/Scope";
 import { ClickableA } from "src/components/ClickableA";
 import { getApiService } from "src/apis";
 import { AccountService } from "src/apis/account/AccountService";
+import { useLocalizedNotification } from "src/utils/useLocalizedNotification";
 
 const root = lang.nav.scopeIndicator;
 
@@ -18,7 +19,7 @@ export const ScopeIndicator: React.FC = () => {
   const userStore = useStore(UserStore);
   const { user } = userStore;
 
-  const [api, contextHolder] = notification.useNotification();
+  const [api, contextHolder] = useLocalizedNotification();
 
   if (!user) { return null; }
 
@@ -29,7 +30,7 @@ export const ScopeIndicator: React.FC = () => {
       <ClickableA>
         <BookOutlined /> <Localized id={root.system} />
       </ClickableA>
-    )
+    );
   }
 
   const projectScopes = availableScopes.filter((x) => x.projectId)
@@ -38,10 +39,10 @@ export const ScopeIndicator: React.FC = () => {
   const menuItems = [] as React.ReactNode[];
 
   const scopeNameWithRole = (scope: Scope) => (
-    <>
+    <Fragment key={scopeId(scope)}>
       {scopeName(scope)}
       {scope.role === "admin" ? <> (<Localized id={root.admin} />)</> : null}
-    </>
+    </Fragment>
   );
 
   const onChange = (scope: Scope) => async () => {
@@ -51,13 +52,12 @@ export const ScopeIndicator: React.FC = () => {
     try {
       const resp = await apiService.changeScope(scope);
       userStore.login({ ...user, token: resp.token, scope });
-
       api.success({
-        message: <Localized id={root.success} />,
+        messageId: root.success,
       });
     } catch (e) {
       api.error({
-        message: <Localized id={root.fail} />,
+        messageId: root.fail,
       });
     } finally {
       setChangingTo(null);
@@ -78,9 +78,8 @@ export const ScopeIndicator: React.FC = () => {
   }
 
   if (domainScopes.length > 0) {
-
     menuItems.push(
-      <Menu.Divider />,
+      <Menu.Divider key="domainDivider" />,
       <Menu.Item key="domainPrompt" disabled={true}>
         <Localized id={root.domains} />
       </Menu.Item>
@@ -109,7 +108,7 @@ export const ScopeIndicator: React.FC = () => {
         <DownOutlined />
       </ClickableA>
     </Dropdown>
-  )
+  );
 
 
 }
