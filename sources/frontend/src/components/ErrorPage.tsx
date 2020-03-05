3f864bc476a65eb9ccf884bced0aaf5bb2096e71
Chen Junda <ddadaal@outlook.com>
2020-03-05 10:03:23 +0800

    restore error page

--- sources/frontend/src/components/ErrorPage.tsx
+++ sources/frontend/src/components/ErrorPage.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Result, Button } from "antd";
-import { RouteComponentProps, navigate, Link } from "@reach/router";
+import { RouteComponentProps, Link } from "@reach/router";
 import { lang, Localized } from "src/i18n";
 
 const root = lang.nav.errorPage;
@@ -8,18 +8,17 @@ const root = lang.nav.errorPage;
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const ErrorPage = (_: RouteComponentProps) => {
   return (
-    <div>404</div>
-    // <Result
-    //   status={404}
-    //   title={<Localized id={root.title} />}
-    //   subTitle={<Localized id={root.description} />}
-    //   extra={(
-    //     <Button type="primary" >
-    //       <Link to="/resources">
-    //         <Localized id={root.backToHome} />
-    //       </Link>
-    //     </Button>
-    //   )}
-    // />
+    <Result
+      status="404"
+      title={<Localized id={root.title} />}
+      subTitle={<Localized id={root.description} />}
+      extra={(
+        <Button type="primary" >
+          <Link to="/resources">
+            <Localized id={root.backToHome} />
+          </Link>
+        </Button>
+      )}
+    />
   )
 }
