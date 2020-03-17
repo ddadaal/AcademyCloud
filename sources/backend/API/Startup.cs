90dc1a28e489ab1e870002c6c05c0d6a16cc7be3
Chen Junda <ddadaal@outlook.com>
2020-03-17 14:52:33 +0800

    add input token on swagger page

--- sources/backend/API/Startup.cs
+++ sources/backend/API/Startup.cs
@@ -87,6 +87,16 @@ namespace AcademyCloud.API
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "AcademyCloud API", Version = "v1" });
                 c.CustomSchemaIds(x => x.FullName);
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = @"JWT Authorization header using the Bearer scheme. \r\n\r\n 
+                      Enter 'Bearer' [space] and then your token in the text input below.
+                      \r\n\r\nExample: 'Bearer 12345abcdef'",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
             });
         }
 
