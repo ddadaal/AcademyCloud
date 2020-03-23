0e4f5d4282abafd783cf134f93b79a32d70780a0
Chen Junda <ddadaal@outlook.com>
2020-03-23 15:30:04 +0800

    update date format

--- sources/frontend/src/i18n/LocalizedDate.tsx
+++ sources/frontend/src/i18n/LocalizedDate.tsx
@@ -3,23 +3,15 @@ import { useStore } from 'simstate';
 import { I18nStore } from '.';
 import dayjs from "dayjs";
 
-import "dayjs/locale/en";
-import "dayjs/locale/zh-cn";
-
-dayjs.locale("en");
-
 interface Props {
   dateTimeString: string;
 }
 
 export const LocalizedDate: React.FC<Props> = ({ dateTimeString }) => {
-  const i18nStore = useStore(I18nStore);
-
-  const locale = useMemo(() => i18nStore.currentLanguage.metadata.dayjsLocale, [i18nStore.currentLanguage]);
 
   return (
     <>
-      {dayjs(dateTimeString).locale(locale).format()}
+      {dayjs(dateTimeString).format("YYYY-MM-DD HH:mm:ss")}
     </>
   )
 }
