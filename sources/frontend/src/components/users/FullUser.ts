308a39d20761ba2c799ac095af65fbf426070a09
Chen Junda <ddadaal@outlook.com>
2020-03-23 20:57:03 +0800

    fix userrole

--- sources/frontend/src/components/users/FullUser.ts
+++ sources/frontend/src/components/users/FullUser.ts
@@ -9,8 +9,8 @@ export interface FullUser extends User {
 
 export function mergeInformation(admins: User[], members: User[], resources: { [userId: string]: Resources } = {}): FullUser[] {
   return [
-    ...admins.map((x) => ({ ...x, role: "admin" as UserRole, resources: resources[x.id] })),
-    ...members.map((x) => ({ ...x, role: "member" as UserRole, resources: resources[x.id] })),
+    ...admins.map((x) => ({ ...x, role: UserRole.Admin, resources: resources[x.id] })),
+    ...members.map((x) => ({ ...x, role: UserRole.Member, resources: resources[x.id] })),
   ];
 
 }
