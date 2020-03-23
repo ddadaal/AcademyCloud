ca10c037680121eaeab475ff3b02f657a7dd422c
Chen Junda <ddadaal@outlook.com>
2020-03-23 15:12:50 +0800

    update compose up script

--- sources/backend/compose-up.ps1
+++ sources/backend/compose-up.ps1
@@ -1,1 +1,11 @@
-docker-compose -f docker-compose.yml -f docker-compose.override.yml up --build
+param(
+    [switch]$build = $false
+)
+
+$base="docker-compose -f docker-compose.yml -f docker-compose.override.yml up"
+
+if ($build) {
+    Invoke-Expression "$base --build"
+} else {
+    Invoke-Expression "$base"
+}
